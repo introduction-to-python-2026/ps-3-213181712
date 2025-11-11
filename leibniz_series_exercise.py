b50362c1207dd92065a7fe3dde22c2d61e19e1d7
213181712 <62132666+213181712@users.noreply.github.com>
2025-11-11 15:28:09 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
     sum_of_series = 0
-    for i in range(n_terms):
+    for i in range(0,n_terms,1):
         sum_of_series = (-1)**i / (1 + 2*i)
     return 4 * sum_of_series
